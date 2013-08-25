a1351579495abd212e469931b36d74636b5052e3
Dany <danyshaanan@gmail.com>
2013-08-25 23:27:30 +0300

    small improvements to minimal

--- python/minimal.py
+++ python/minimal.py
@@ -1,10 +1,13 @@
 import PIL.Image
 s=255
-i,r=PIL.Image.new('L',(2*s,)*2),range(-s,s)
-for b in r:
- for a in r:
+i=PIL.Image.new('L',(2*s,)*2)
+r=range(-s,s)
+for a in r:
+ for b in r:
   o=x=(a+b*1j)/99
   h=s
-  while abs(x)<h:h,x=h-9,x*x+o
+  while abs(x)<h:
+   h-=9
+   x=x*x+o
   i.load()[s+a,s+b]=h
 i.show()
