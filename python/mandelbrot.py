20a5de0f533be830a4c6fba28e8ce61225444358
Dany <danyshaanan@gmail.com>
2013-08-25 21:15:23 +0300

    refactored variables

--- python/mandelbrot.py
+++ python/mandelbrot.py
@@ -1,29 +1,28 @@
 #!/bin/python
 import math, PIL.Image
 
-pixel_size = 320*2
-uclid_size = 3.2
-asft, bsft = -0.5,-0.0
-radius     = 2**3
-rounds     = 41
-continuous = True
+w, h = 640, 640
+x, y = -0.5, 0
+zoom = 200
+radius        = 2**3
+maxIterations = 41
+continuous    = True
 
-u2p = uclid_size/pixel_size
-image = PIL.Image.new('L',(pixel_size,pixel_size),0)
+image = PIL.Image.new('L',(w,h),0)
 
-bimag = [(b,(b-pixel_size/2)*u2p+bsft) for b in range(pixel_size)]
-areal = [(a,(a-pixel_size/2)*u2p+asft) for a in range(pixel_size)]
+areal = [(a,(a - w/2.)/zoom + x) for a in range(w)]
+bimag = [(b,(b - h/2.)/zoom + y) for b in range(h)]
 pixel = image.load()
 
 for (b,imag) in bimag:
   for (a,real) in areal:
     v = cv = real + imag*1j
-    n = 0
-    while n<rounds and abs(v)<radius:
+    i = 0
+    while i<maxIterations and abs(v)<radius:
       v = v*v + cv
-      n += 1
-    if n<rounds:
-      pixel[a,b] = 127+127*math.sin(n - math.log(abs(v),radius)) if continuous else 6*n
+      i += 1
+    if i<maxIterations:
+      pixel[a,b] = 127+127*math.sin(i - math.log(abs(v),radius)) if continuous else 6*i
 
 image.show()
 # image.save("mandelbrot.png")
